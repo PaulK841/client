b0a5aece640510984a6f17fa283a42d58cd73a3d
PaulK841 <118380695+PaulK841@users.noreply.github.com>
2025-09-19 00:41:26 +0200

    zea
    zar

--- server/routes/paypalRoutes.js
+++ server/routes/paypalRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createOrder, captureOrder } = require('../controllers/paypalController.js');
-const { protect } = require('../middleware/authMiddleware.js');
+const protect = require('../middleware/authMiddleware.js');
 
 // Ces routes sont protégées : seul un utilisateur connecté peut créer une commande.
 router.post('/orders', protect, createOrder);
